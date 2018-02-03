050d51f83d7ebd635d04cac218e6b23ad4fdccd7
Aleksandr <asdiamond@miners.utep.edu>
2018-02-03 16:34:59 -0700

    ported a bit more.

--- tools/cache.py
+++ tools/cache.py
@@ -19,13 +19,43 @@ class MyCache:
     def __init__(self, size):
         self.size = size
 
-    def addEntry(key, value):
-        if(len(lru) >= size):
-            evict()
+    def printcache(self):
+        """
+        should print each element of the cache and its val
+        """
+        for x,y in self.cache:
+            print(x, y)
+        print("Called printCache()")
 
-    def evict():
-        print ("Called evict")
+    def addentry(self, key, value):
+        """
+        :param key: a string
+        :param value:should be a MyCacheableObject
+        """
+        try:
+            if len(self.cache) > self.size:
+                self.evict()
+            value.setcache(self)
+            self.cache[key] = value
+            self.lru.insert(0, value)
+        except OSError: #will this type of exception be thrown?
+            self.lru.remove(value) #hopefully does nothing if doesnt exist
+            print("Error")
+
+    def getentry(self, key):
+        if self.cache.__contains__(key) :
+            mco = self.cache[key]
+            self.lru.remove(self.cache[key]) #remove by key given
+
+    def evict(self):
+        pass
 
 
+
+
+class MyCacheableObject:
+
+    def setCache(self):
+        print ("called setcache()")
 
 if __name__ == "__main__" : main()
