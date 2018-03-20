c354475d29b7b7df24ae5f829f8eae7fb43ef49b
aleksandr diamond <asdiamond@miners.utep.edu>
2018-03-19 23:27:36 -0600

    A clean slate for java port

--- tools/fire_log.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#export MOZ_LOG=timestamp,rotate:200,nsHttp:5,nsSocketTransport:5,nsStreamPump:5,nsHostResolver:5
-#this command was the original, removing all other params leaves us with cache events
-#export MOZ_LOG=timestamp,rotate:50,nsHttp:5,nsSocketTransport:5,nsStreamPump:5,nsHostResolver:5,cache2:5
-export MOZ_LOG=timestamp,rotate:50,cache2:5
-export MOZ_LOG_FILE=/tmp/cache-log.txt
-cd /usr/bin/
-./firefox
--- tools/printer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd ../fire_logs/
-cat * | python ../tools/log_parser_access.py
